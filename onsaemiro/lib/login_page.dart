a67a12d4d66960c00ce5bfad846e3e434f1422f5
hhs0579 <hhs0579@naver.com>
2021-12-22 17:27:36 +0900

    dd

--- onsaemiro/lib/login_page.dart
+++ onsaemiro/lib/login_page.dart
@@ -17,6 +17,20 @@ class _LoginPageState extends State<LoginPage> {
             height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width,
             color: Colors.grey,
+          ),
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+            child: (Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: '이메일을 적어주세요',
+                  ),
+                ),
+              ],
+            )),
           )
         ],
       ),
