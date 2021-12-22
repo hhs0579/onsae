f090878701f03800aa72e52435ae71e84a512b3c
hhs0579 <hhs0579@naver.com>
2021-12-22 20:39:21 +0900

    nick

--- onsaemiro/lib/screens/starting_pages/nick_name-page.dart
+++ onsaemiro/lib/screens/starting_pages/nick_name-page.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class nickNamePage extends StatelessWidget {
@@ -5,6 +7,30 @@ class nickNamePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Container(
+            margin: EdgeInsets.only(top: 40),
+            child: Column(children: [
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Image.asset('assets/Vector.png'),
+                  ),
+                  SizedBox(
+                    width: 95,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              Text('온새미로에서 사용할 닉네임을 설정해주세요.'),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 100, top: 20, bottom: 20, right: 100),
+                child: TextFormField(),
+              ),
+            ])));
   }
 }
