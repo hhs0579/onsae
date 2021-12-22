5c287bbf946ec498aae4d63222c9f414cdfc3d3e
hhs0579 <hhs0579@naver.com>
2021-12-22 18:11:24 +0900

    dsaf

--- onsaemiro/lib/login_page.dart
+++ onsaemiro/lib/login_page.dart
@@ -6,55 +6,51 @@ class loginpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            color: Colors.grey,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                  height: 300,
-                  width: 300,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(20))),
-                  child: Column(
-                    children: [
-                      Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 8, vertical: 16)),
-                      TextField(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: Colors.grey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+                height: 300,
+                width: 300,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: Column(
+                  children: [
+                    Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8, vertical: 16)),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: '이메일을 적어주세요',
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 16),
+                      child: TextField(
                         decoration: InputDecoration(
                           border: OutlineInputBorder(),
-                          hintText: '이메일을 적어주세요',
+                          hintText: '비밀번호',
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 8, vertical: 16),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            hintText: '비밀번호',
-                          ),
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          TextButton(onPressed: () {}, child: Text('회원가입')),
-                          TextButton(onPressed: () {}, child: Text('비밀번호 찾기')),
-                        ],
-                      )
-                    ],
-                  )),
-            ],
-          ),
-        ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        TextButton(onPressed: () {}, child: Text('회원가입')),
+                        TextButton(onPressed: () {}, child: Text('비밀번호 찾기')),
+                      ],
+                    )
+                  ],
+                )),
+          ],
+        ),
       ),
     );
   }
