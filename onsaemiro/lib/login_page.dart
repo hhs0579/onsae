68c62d3a3a90d15bf4762faed56a8a933f5af929
hhs0579 <hhs0579@naver.com>
2021-12-22 17:46:47 +0900

    dd

--- onsaemiro/lib/login_page.dart
+++ onsaemiro/lib/login_page.dart
@@ -14,48 +14,53 @@ class loginpage extends StatelessWidget {
             color: Colors.grey,
           ),
           Container(
+            height: 300,
+            width: 300,
             decoration: BoxDecoration(
+                color: Colors.white,
                 borderRadius: BorderRadius.all(Radius.circular(20))),
-            child: (Column(
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: '이메일을 적어주세요',
-                  ),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: '비밀번호',
-                    ),
-                  ),
-                ),
-                Row(
+            child: Align(
+                alignment: Alignment.center,
+                child: Column(
                   children: [
-                    TextButton(
-                        onPressed: () {
-                          Visibility(
-                              child: Column(
-                            children: [
-                              TextField(
-                                decoration: InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  hintText: '비밀번호 확인',
-                                ),
-                              ),
-                            ],
-                          ));
-                        },
-                        child: Text('회원가입')),
-                    TextButton(onPressed: () {}, child: Text('비밀번호 찾기')),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: '이메일을 적어주세요',
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 16),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: '비밀번호',
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        TextButton(
+                            onPressed: () {
+                              Visibility(
+                                  child: Column(
+                                children: [
+                                  TextField(
+                                    decoration: InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      hintText: '비밀번호 확인',
+                                    ),
+                                  ),
+                                ],
+                              ));
+                            },
+                            child: Text('회원가입')),
+                        TextButton(onPressed: () {}, child: Text('비밀번호 찾기')),
+                      ],
+                    )
                   ],
-                )
-              ],
-            )),
+                )),
           )
         ],
       ),
