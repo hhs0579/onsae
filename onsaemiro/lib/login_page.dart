7897a0099fa26c3885878b308c07cff6f327c287
hhs0579 <hhs0579@naver.com>
2021-12-22 18:05:48 +0900

    dsd

--- onsaemiro/lib/login_page.dart
+++ onsaemiro/lib/login_page.dart
@@ -13,55 +13,47 @@ class loginpage extends StatelessWidget {
             width: MediaQuery.of(context).size.width,
             color: Colors.grey,
           ),
-          Container(
-            height: 300,
-            width: 300,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(20))),
-            child: Align(
-                alignment: Alignment.center,
-                child: Column(
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: '이메일을 적어주세요',
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8, vertical: 16),
-                      child: TextField(
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                  child: Column(
+                    children: [
+                      Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 8, vertical: 16)),
+                      TextField(
                         decoration: InputDecoration(
                           border: OutlineInputBorder(),
-                          hintText: '비밀번호',
+                          hintText: '이메일을 적어주세요',
                         ),
                       ),
-                    ),
-                    Row(
-                      children: [
-                        TextButton(
-                            onPressed: () {
-                              Visibility(
-                                  child: Column(
-                                children: [
-                                  TextField(
-                                    decoration: InputDecoration(
-                                      border: OutlineInputBorder(),
-                                      hintText: '비밀번호 확인',
-                                    ),
-                                  ),
-                                ],
-                              ));
-                            },
-                            child: Text('회원가입')),
-                        TextButton(onPressed: () {}, child: Text('비밀번호 찾기')),
-                      ],
-                    )
-                  ],
-                )),
-          )
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8, vertical: 16),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: '비밀번호',
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          TextButton(onPressed: () {}, child: Text('회원가입')),
+                          TextButton(onPressed: () {}, child: Text('비밀번호 찾기')),
+                        ],
+                      )
+                    ],
+                  )),
+            ],
+          ),
         ],
       ),
     );
