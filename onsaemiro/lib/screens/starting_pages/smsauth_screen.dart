4bcc508af9b4c000f84ad03eb1bb29d7f80a9065
2Space9 <sjk338285@gmail.com>
2021-12-22 22:33:50 +0900

    1222 final

--- onsaemiro/lib/screens/starting_pages/smsauth_screen.dart
+++ onsaemiro/lib/screens/starting_pages/smsauth_screen.dart
@@ -1,6 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import 'agree_page.dart';
 import 'connect_screen.dart';
 
@@ -11,23 +11,51 @@ class SmsAuthScreen extends StatefulWidget {
   _SmsAuthScreenState createState() => _SmsAuthScreenState();
 }
 
-_connectbutton(text, onPressed) {
-  return Container(
-    width: 275,
-    height: 60,
-    margin: EdgeInsets.only(bottom: 20),
-    child: TextButton(
-        onPressed: onPressed,
-        style: TextButton.styleFrom(
-            primary: Colors.black,
-            shape: RoundedRectangleBorder(
-                side: BorderSide(color: Colors.green, width: 1.5),
-                borderRadius: BorderRadius.circular(30))),
-        child: Text(text)),
-  );
-}
-
 class _SmsAuthScreenState extends State<SmsAuthScreen> {
+  Timer? _timer;
+  var _time = 0;
+  bool isSmsAuth = false;
+
+  _connectbutton(text, onPressed) {
+    return Container(
+      width: 275,
+      height: 60,
+      margin: EdgeInsets.only(bottom: 20),
+      child: TextButton(
+          onPressed: onPressed,
+          style: TextButton.styleFrom(
+              primary: Colors.black,
+              shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Colors.green, width: 1.5),
+                  borderRadius: BorderRadius.circular(30))),
+          child: Text(text)),
+    );
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    dispose();
+  }
+
+  void _timeStart() {
+    _time = 180;
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        _time--;
+        if (_time <= 0) {
+          _time = 0;
+        }
+      });
+    });
+  }
+
+  String _viewTime(int time) {
+    final minutes = ((time / 60) % 60).floor().toString();
+    final seconds = (time % 60).floor().toString().padLeft(2, '0');
+    return '$minutes:$seconds';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,22 +86,111 @@ class _SmsAuthScreenState extends State<SmsAuthScreen> {
                       textAlign: TextAlign.center,
                       style: TextStyle(fontSize: 18)),
                 ),
-                Container(
-                  width: 200,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.zero,
-                      enabledBorder: UnderlineInputBorder(
-                          borderSide:
-                              BorderSide(color: Colors.black26, width: 1.0)),
-                      focusedBorder: UnderlineInputBorder(
-                          borderSide:
-                              BorderSide(color: Colors.green, width: 1.5)),
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Container(
+                    width: 160,
+                    height: 30,
+                    child: TextField(
+                      style: TextStyle(fontSize: 20, height: 1.5),
+                      decoration: InputDecoration(
+                        isDense: true,
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+                        border: InputBorder.none,
+                      ),
+                      keyboardType: TextInputType.phone,
                     ),
+                  ),
+                  Container(
+                    width: 40,
+                    height: 20,
+                    child: TextButton(
+                        onPressed: () {
+                          _timeStart();
+                          isSmsAuth = true;
+                        },
+                        style: TextButton.styleFrom(
+                            padding: EdgeInsets.zero,
+                            primary: Colors.white,
+                            backgroundColor: Colors.green,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8))),
+                        child: Text(
+                          '인증',
+                          style: TextStyle(fontSize: 11),
+                        )),
+                  )
+                ]),
+                Container(
+                  color: Colors.green,
+                  width: 200,
+                  height: 2,
+                ),
+                SizedBox(height: 10),
+                Visibility(
+                  visible: isSmsAuth == true,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 125,
+                          height: 30,
+                          child: TextField(
+                            style: TextStyle(fontSize: 20, height: 1.5),
+                            decoration: InputDecoration(
+                              isDense: true,
+                              contentPadding: EdgeInsets.symmetric(
+                                  horizontal: 0, vertical: 0),
+                              border: InputBorder.none,
+                            ),
+                            keyboardType: TextInputType.phone,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 5),
+                          child: Text(_viewTime(_time),
+                              style: TextStyle(
+                                color: Colors.grey,
+                              )),
+                        ),
+                        Container(
+                          width: 40,
+                          height: 20,
+                          child: TextButton(
+                              onPressed: () {},
+                              style: TextButton.styleFrom(
+                                  padding: EdgeInsets.zero,
+                                  primary: Colors.white,
+                                  backgroundColor: Colors.green,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8))),
+                              child: Text(
+                                '확인',
+                                style: TextStyle(fontSize: 11),
+                              )),
+                        )
+                      ]),
+                ),
+                Visibility(
+                  visible: isSmsAuth == true,
+                  child: Container(
+                    color: Colors.green,
+                    width: 200,
+                    height: 2,
+                  ),
+                ),
+                Visibility(
+                  visible: isSmsAuth == true,
+                  child: Container(
+                    width: 220,
+                    margin: EdgeInsets.only(top: 5),
+                    child: Text('전송된 4자리 코드 입력해주세요.',
+                        style: TextStyle(fontSize: 15),
+                        textAlign: TextAlign.center),
                   ),
                 ),
                 SizedBox(height: 250),
-                _connectbutton('동의하고 진행하기', () {
+                _connectbutton('동의하고 계속 진행합니다', () {
                   Get.to(agreePage());
                 })
               ],
