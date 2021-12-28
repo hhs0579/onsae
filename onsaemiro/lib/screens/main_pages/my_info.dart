f55b1216daee10abb64338fb06501da0aa17c21e
NaJunhyeok <njs4922@gmail.com>
2021-12-28 14:53:28 +0900

    Update my_info.dart

--- onsaemiro/lib/screens/main_pages/my_info.dart
+++ onsaemiro/lib/screens/main_pages/my_info.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_connect/http/src/utils/utils.dart';
 
@@ -12,26 +13,91 @@ _connectbutton(text, onPressed) {
   return Container(
     width: 355,
     height: 40,
-    margin: EdgeInsets.all(10),
-    child: TextButton(
-        onPressed: onPressed,
-        style: TextButton.styleFrom(
-            primary: Colors.black,
-            shape: RoundedRectangleBorder(
-                side: BorderSide(color: Colors.green, width: 2),
-                borderRadius: BorderRadius.circular(7))),
+    margin: EdgeInsets.all(5),
+    decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.green,
+        ),
+        borderRadius: BorderRadius.circular(7.0)),
+    child: Container(
         child: Row(
-          children: [
-            SizedBox(
-              width: 28,
+      children: [
+        SizedBox(
+          width: 25,
+        ),
+        Text(text),
+        SizedBox(
+          width: 192,
+        ),
+        IconButton(
+            onPressed: onPressed, icon: Image.asset('assets/바로가기 버튼.png'))
+      ],
+    )),
+  );
+}
+
+spending_info(where, which, when, price) {
+  return Column(
+    children: [
+      Row(
+        children: [
+          SizedBox(
+            width: 20,
+          ),
+          Text(
+            where,
+            style: TextStyle(
+              fontSize: 13,
+              fontWeight: FontWeight.w700,
             ),
-            Text(text),
-            SizedBox(
-              width: 195,
-            ),
-            Image.asset('assets/바로가기 버튼.png')
-          ],
-        )),
+          ),
+          SizedBox(
+            width: 250,
+          )
+        ],
+      ),
+      Row(
+        children: [
+          SizedBox(
+            width: 20,
+          ),
+          Text(
+            which,
+            style: TextStyle(fontSize: 13, fontWeight: FontWeight.w500),
+          ),
+          SizedBox(
+            width: 3,
+          ),
+          Column(
+            children: [
+              SizedBox(height: 5),
+              Text(
+                when,
+                style: TextStyle(fontSize: 8, fontWeight: FontWeight.w500),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 80,
+          ),
+          Text(price)
+        ],
+      )
+    ],
+  );
+}
+
+info_button(text, onPressed) {
+  return TextButton(
+    child: Text(
+      text,
+      style: TextStyle(
+        fontSize: 8,
+        fontWeight: FontWeight.w700,
+        color: Color(0xff59595980),
+      ),
+    ),
+    onPressed: onPressed,
   );
 }
 
@@ -62,112 +128,107 @@ class _MyInfoPageState extends State<MyInfoPage> {
           ],
         ),
       ),
-      body: Column(
-        children: [
-          Container(
-            width: 355,
-            height: 170,
-            decoration: BoxDecoration(
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              width: 355,
+              height: 170,
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2,
+                    color: Colors.green,
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/프로필.png'),
+                  Text(
+                    '온새미로',
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
+                  ),
+                  Text('onsemiro.gmail.com'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('좋아요', style: TextStyle(fontSize: 13)),
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Text(
+                        '100',
+                        style: TextStyle(
+                          fontSize: 11,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            _connectbutton('회원정보 수정', () {}),
+            _connectbutton('상점 후기 작성', () {}),
+            _connectbutton('상품 후기 작성', () {}),
+            SizedBox(
+              height: 5,
+            ),
+            Container(
+              width: 355,
+              height: 190,
+              decoration: BoxDecoration(
                 border: Border.all(
                   width: 2,
                   color: Colors.green,
                 ),
-                borderRadius: BorderRadius.all(Radius.circular(20.0))),
-            child: Column(
+                borderRadius: BorderRadius.circular(19),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    '최근 지출내역',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  spending_info(
+                      '망넛이네', '약콩두유 100 (3개)', '2021-11-18(목)', '3,300원'),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  spending_info(
+                      '망넛이네', '약콩두유 100 (3개)', '2021-11-18(목)', '3,300원'),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  spending_info(
+                      '망넛이네', '약콩두유 100 (3개)', '2021-11-18(목)', '3,300원')
+                ],
+              ),
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image.asset('assets/프로필.png'),
-                Text(
-                  '온새미로',
-                  style: TextStyle(
-                    fontSize: 24,
-                  ),
-                ),
-                Text('data'),
-                Text('data'),
+                info_button('고객센터', () {}),
+                info_button('로그아웃', () {}),
+                info_button('사업자 사용자 전환', () {}),
               ],
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          _connectbutton('회원정보 수정', () {}),
-          _connectbutton('회원정보 수정', () {}),
-          _connectbutton('회원정보 수정', () {}),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            width: 355,
-            height: 170,
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 2,
-                color: Colors.green,
-              ),
-              borderRadius: BorderRadius.circular(19),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  '최근 지출내역',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  children: [
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Text(
-                      '망넛이네',
-                      style: TextStyle(
-                        fontSize: 13,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 250,
-                    )
-                  ],
-                ),
-                Row(
-                  children: [
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Text(
-                      '약콩두유 100 (3개)',
-                      style:
-                          TextStyle(fontSize: 13, fontWeight: FontWeight.w500),
-                    ),
-                    SizedBox(
-                      width: 3,
-                    ),
-                    Text(
-                      '2021-11-18(목)',
-                      style:
-                          TextStyle(fontSize: 8, fontWeight: FontWeight.w500),
-                    ),
-                    SizedBox(
-                      width: 80,
-                    ),
-                    Text('3,300원')
-                  ],
-                )
-              ],
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
