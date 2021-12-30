623f4e2efbda9d0a6390dacb5a4ec88c205f9ed8
NaJunhyeok <njs4922@gmail.com>
2021-12-30 20:56:09 +0900

    지역별 가로뷰까지 미완성

---
+++ onsaemiro/lib/screens/access_pages/local_Seoul.dart
@@ -0,0 +1,316 @@
+import 'package:flutter/material.dart';
+
+class localSeoulPage extends StatefulWidget {
+  const localSeoulPage({Key? key}) : super(key: key);
+
+  @override
+  _localSeoulPageState createState() => _localSeoulPageState();
+}
+
+imagestackWidget(shop, phone, where, insta, image) {
+  return Container(
+    height: 153,
+    width: 153,
+    child: Stack(
+      children: [
+        Positioned(
+          top: 70,
+          child: Container(
+            height: 72,
+            width: 153,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 20,
+                  spreadRadius: 2,
+                ),
+              ],
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),
+            child: Column(
+              children: [
+                Text(
+                  shop,
+                  style: TextStyle(fontSize: 10),
+                ),
+                Text(
+                  phone,
+                  style: TextStyle(fontSize: 8),
+                ),
+                Text(
+                  insta,
+                  style: TextStyle(fontSize: 6),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Ink(
+          height: 102,
+          width: 156,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(image),
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(14))),
+          child: InkWell(
+            borderRadius: BorderRadius.all(Radius.circular(14)),
+            onTap: () {},
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+class _localSeoulPageState extends State<localSeoulPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leadingWidth: 8,
+        toolbarHeight: 190,
+        backgroundColor: Colors.white,
+        elevation: 0.5,
+        title: Center(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  SizedBox(
+                    width: 321,
+                    height: 31,
+                    child: TextField(
+                      style: TextStyle(fontSize: 11),
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          filled: true,
+                          fillColor: Color.fromRGBO(162, 191, 98, 0.2),
+                          contentPadding: EdgeInsets.symmetric(vertical: 8),
+                          prefixIcon: IconButton(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 8, horizontal: 11),
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.search,
+                                color: Color.fromRGBO(162, 191, 98, 1),
+                                size: 15,
+                              )),
+                          hintText: '상품검색',
+                          hintStyle: TextStyle(
+                              fontSize: 11,
+                              color: Color.fromRGBO(162, 191, 98, 0.5)),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(27.0)),
+                            borderSide: BorderSide(color: Color(0xffffffff)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(27.0)),
+                              borderSide:
+                                  BorderSide(color: Color(0xffA2BF62)))),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Image.asset('assets/장바구니 (누런녹색).png'),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              Container(
+                  child: Row(
+                children: [
+                  IconButton(
+                    padding: EdgeInsets.fromLTRB(20, 11, 100.59, 0),
+                    onPressed: () {},
+                    icon: Image.asset('assets/Vector(누런녹색).png'),
+                    iconSize: 12,
+                  ),
+                  Text(
+                    'access',
+                    style: TextStyle(
+                        fontSize: 36, color: Color.fromRGBO(162, 191, 98, 1)),
+                  )
+                ],
+              )),
+              Container(
+                  padding: EdgeInsets.fromLTRB(135, 10, 0, 0),
+                  child: Row(
+                    children: [
+                      Text(
+                        '강남구 역삼동',
+                        style:
+                            TextStyle(fontSize: 15, color: Color(0xff595959)),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Image.asset('assets/화살표.png'),
+                        iconSize: 12,
+                      ),
+                    ],
+                  )),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        '주문 많은순',
+                        style: TextStyle(
+                            fontSize: 13,
+                            color: Color.fromRGBO(89, 89, 89, 0.5)),
+                      )),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        '만족도순',
+                        style: TextStyle(
+                            fontSize: 13,
+                            color: Color.fromRGBO(89, 89, 89, 0.5)),
+                      )),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        '거리순',
+                        style: TextStyle(
+                            fontSize: 13,
+                            color: Color.fromRGBO(89, 89, 89, 0.5)),
+                      )),
+                  TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        '온새미로 인증',
+                        style: TextStyle(
+                            fontSize: 13,
+                            color: Color.fromRGBO(89, 89, 89, 0.5)),
+                      )),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              height: 245.91,
+              width: 234,
+              padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: 113.09,
+                    child: Container(
+                      height: 110,
+                      width: 234,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey,
+                            blurRadius: 20,
+                            spreadRadius: 2,
+                          ),
+                        ],
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            '디어비건베이커리',
+                            style: TextStyle(fontSize: 13),
+                          ),
+                          Text(
+                            '02-558-0301',
+                            style: TextStyle(fontSize: 11),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.location_on_outlined),
+                                iconSize: 12,
+                                onPressed: () {},
+                              ),
+                              Text(
+                                '부천 상동',
+                                style: TextStyle(fontSize: 11),
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(0, 0, 0, 13),
+                            child: Text(
+                              'http://wwww.instagram.com/bo.mool_vegan',
+                              style: TextStyle(fontSize: 8),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Ink(
+                    height: 165,
+                    width: 234,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/핫케이크.png'),
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                    child: InkWell(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      onTap: () {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 170,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  imagestackWidget(
+                      '비건 베이커리보물',
+                      '02-558-0301',
+                      '서울 강남구 논현로 67길 11 1층',
+                      'http://www.instagram.com/bo.mool_vegan',
+                      'assets/샌드위치.png'),
+                  SizedBox(width: 26),
+                  imagestackWidget(
+                      '비건 베이커리보물',
+                      '02-558-0301',
+                      '서울 강남구 논현로 67길 11 1층',
+                      'http://www.instagram.com/bo.mool_vegan',
+                      'assets/샌드위치.png'),
+                  SizedBox(width: 26),
+                  imagestackWidget(
+                      '비건 베이커리보물',
+                      '02-558-0301',
+                      '서울 강남구 논현로 67길 11 1층',
+                      'http://www.instagram.com/bo.mool_vegan',
+                      'assets/샌드위치.png'),
+                  SizedBox(width: 26),
+                  imagestackWidget(
+                      '비건 베이커리보물',
+                      '02-558-0301',
+                      '서울 강남구 논현로 67길 11 1층',
+                      'http://www.instagram.com/bo.mool_vegan',
+                      'assets/샌드위치.png'),
+                  SizedBox(width: 26),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- onsaemiro/lib/screens/access_pages/local_select.dart
+++ onsaemiro/lib/screens/access_pages/local_select.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
+
+import 'local_Seoul.dart';
 
 class localSelectPage extends StatefulWidget {
   const localSelectPage({Key? key}) : super(key: key);
@@ -7,73 +12,134 @@ class localSelectPage extends StatefulWidget {
   _localSelectPageState createState() => _localSelectPageState();
 }
 
+local_Stack(where, whereInEnglish, whichPicture, onTap) {
+  return Stack(
+    children: [
+      Positioned(
+        top: 58.5,
+        left: 130,
+        child: Column(
+          children: [
+            Text(where),
+            Text(whereInEnglish),
+          ],
+        ),
+      ),
+      Ink(
+        padding: EdgeInsets.fromLTRB(0, 19, 0, 19),
+        decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(whichPicture),
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(20))),
+        child: InkWell(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          onTap: onTap,
+          child: Container(
+            height: 117,
+            width: 291,
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
 class _localSelectPageState extends State<localSelectPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leadingWidth: 8,
-        toolbarHeight: 160,
-        backgroundColor: Colors.white,
-        actions: [
-          IconButton(
-            padding: EdgeInsets.fromLTRB(10, 6, 10, 84),
-            icon: Image.asset('assets/장바구니 (누런녹색).png'),
-            onPressed: () {},
-          ),
-        ],
-        elevation: 0.5,
-        title: Column(
-          children: [
-            Container(
-              height: 31,
-              child: TextField(
-                style: TextStyle(fontSize: 11),
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    filled: true,
-                    fillColor: Color.fromRGBO(162, 191, 98, 0.2),
-                    contentPadding: EdgeInsets.symmetric(vertical: 8),
-                    prefixIcon: IconButton(
-                        padding:
-                            EdgeInsets.symmetric(vertical: 8, horizontal: 11),
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.search,
-                          color: Color.fromRGBO(162, 191, 98, 1),
-                          size: 15,
-                        )),
-                    hintText: '상품검색',
-                    hintStyle: TextStyle(
-                        fontSize: 11, color: Color.fromRGBO(162, 191, 98, 0.5)),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(27.0)),
-                      borderSide: BorderSide(color: Color(0xffffffff)),
+        appBar: AppBar(
+          leadingWidth: 8,
+          toolbarHeight: 160,
+          backgroundColor: Colors.white,
+          elevation: 0.5,
+          title: Column(
+            children: [
+              Row(
+                children: [
+                  SizedBox(
+                    width: 321,
+                    height: 31,
+                    child: TextField(
+                      style: TextStyle(fontSize: 11),
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          filled: true,
+                          fillColor: Color.fromRGBO(162, 191, 98, 0.2),
+                          contentPadding: EdgeInsets.symmetric(vertical: 8),
+                          prefixIcon: IconButton(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 8, horizontal: 11),
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.search,
+                                color: Color.fromRGBO(162, 191, 98, 1),
+                                size: 15,
+                              )),
+                          hintText: '상품검색',
+                          hintStyle: TextStyle(
+                              fontSize: 11,
+                              color: Color.fromRGBO(162, 191, 98, 0.5)),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(27.0)),
+                            borderSide: BorderSide(color: Color(0xffffffff)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(27.0)),
+                              borderSide:
+                                  BorderSide(color: Color(0xffA2BF62)))),
                     ),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(27.0)),
-                        borderSide: BorderSide(color: Color(0xffA2BF62)))),
+                  ),
+                  IconButton(
+                    icon: Image.asset('assets/장바구니 (누런녹색).png'),
+                    onPressed: () {},
+                  ),
+                ],
               ),
-            ),
-            Container(
-                child: Row(
-              children: [
-                IconButton(
-                  padding: EdgeInsets.fromLTRB(20, 33, 90.59, 34),
-                  onPressed: () {},
-                  icon: Image.asset('assets/Vector(누런녹색).png'),
-                  iconSize: 12,
-                ),
-                Text(
-                  'access',
-                  style: TextStyle(
-                      fontSize: 36, color: Color.fromRGBO(162, 191, 98, 1)),
-                )
-              ],
-            ))
-          ],
+              Container(
+                  child: Row(
+                children: [
+                  IconButton(
+                    padding: EdgeInsets.fromLTRB(20, 33, 90.59, 34),
+                    onPressed: () {},
+                    icon: Image.asset('assets/Vector(누런녹색).png'),
+                    iconSize: 12,
+                  ),
+                  Text(
+                    'access',
+                    style: TextStyle(
+                        fontSize: 36, color: Color.fromRGBO(162, 191, 98, 1)),
+                  )
+                ],
+              ))
+            ],
+          ),
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                local_Stack('서울', 'Seoul', 'assets/서울.png', () {
+                  Get.to(localSeoulPage());
+                }),
+                local_Stack('서울', 'Seoul', 'assets/서울.png', () {}),
+                local_Stack('대구', 'Deagu', 'assets/대구.png', () {}),
+                SizedBox(
+                  height: 14,
+                ),
+                local_Stack('부산', 'Busan', 'assets/부산.png', () {}),
+                local_Stack('부산', 'Busan', 'assets/부산.png', () {}),
+              ],
+            ),
+          ),
+        ));
   }
 }
