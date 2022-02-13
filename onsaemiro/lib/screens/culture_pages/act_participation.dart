60d327605f39a4db40d3ea8a9124604568d9ae70
hhs0579 <hhs0579@naver.com>
2022-02-13 15:57:31 +0900

    ..

--- onsaemiro/lib/screens/culture_pages/act_participation.dart
+++ onsaemiro/lib/screens/culture_pages/act_participation.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class actParticipationPage extends StatefulWidget {
   const actParticipationPage({Key? key}) : super(key: key);
@@ -7,9 +12,84 @@ class actParticipationPage extends StatefulWidget {
   _actParticipationPageState createState() => _actParticipationPageState();
 }
 
+List<String> _arrImageUrls = [];
+
+List<XFile>? imageFileList = [];
+final _picker = ImagePicker();
+FirebaseStorage firebaseStorage = FirebaseStorage.instance;
+
 class _actParticipationPageState extends State<actParticipationPage> {
+  Future<void> _pickedImgs() async {
+    if (imageFileList != null) {
+      imageFileList?.clear();
+    }
+    try {
+      final List<XFile>? imgs = await _picker.pickMultiImage(
+        imageQuality: 70,
+        maxWidth: 1000,
+      );
+
+      if (imgs!.isNotEmpty) {
+        imageFileList!.addAll(imgs);
+      }
+      print(imgs.length.toString());
+    } catch (e) {
+      print(e.toString());
+    }
+    setState(() {});
+  }
+
+  Future<String> uploadFile(XFile _image) async {
+    String imageurls = _image.name;
+    Reference reference =
+        FirebaseStorage.instance.ref().child('post').child(imageurls);
+
+    UploadTask uploadTask = reference.putFile(File(_image.path));
+    await uploadTask.whenComplete(() {});
+    return await reference.getDownloadURL();
+  }
+
+  Future uploadFunction(List<XFile> _images) async {
+    for (int i = 0; i < _images.length; i++) {
+      var imageUrl = await uploadFile(_images[i]);
+      _arrImageUrls.add(imageUrl.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    bool isPadMode = MediaQuery.of(context).size.width > 300;
+    List<Widget> _boxContents = [
+      IconButton(
+          onPressed: () {
+            _pickedImgs();
+          },
+          icon: Container(
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.6), shape: BoxShape.circle),
+              child: Icon(
+                CupertinoIcons.camera,
+                color: Theme.of(context).colorScheme.primary,
+              ))),
+      Container(),
+      Container(),
+      imageFileList!.length <= 4
+          ? Container()
+          : FittedBox(
+              child: Container(
+                  padding: EdgeInsets.all(6),
+                  decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.6),
+                      shape: BoxShape.circle),
+                  child: Text(
+                    '+${(imageFileList!.length - 4).toString()}',
+                    style: Theme.of(context)
+                        .textTheme
+                        .subtitle2
+                        ?.copyWith(fontWeight: FontWeight.w800),
+                  ))),
+    ];
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 138,
@@ -49,50 +129,30 @@ class _actParticipationPageState extends State<actParticipationPage> {
                         borderSide: BorderSide(color: Colors.green))),
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  margin: EdgeInsets.fromLTRB(20.76, 19, 16.24, 18),
-                  width: 56,
-                  height: 56,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.green),
-                    borderRadius: BorderRadius.all(Radius.circular(16)),
-                  ),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: Image.asset('assets/이미지 추가.png'),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(20.76, 19, 16.24, 18),
-                  width: 56,
-                  height: 56,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.green),
-                    borderRadius: BorderRadius.all(Radius.circular(16)),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(20.76, 19, 16.24, 18),
-                  width: 56,
-                  height: 56,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.green),
-                    borderRadius: BorderRadius.all(Radius.circular(16)),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(20.76, 19, 16.24, 18),
-                  width: 56,
-                  height: 56,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.green),
-                    borderRadius: BorderRadius.all(Radius.circular(16)),
-                  ),
-                ),
-              ],
+            GridView.count(
+              shrinkWrap: true,
+              padding: EdgeInsets.all(2),
+              crossAxisCount: isPadMode ? 4 : 2,
+              mainAxisSpacing: 5,
+              crossAxisSpacing: 5,
+              children: List.generate(
+                  4,
+                  (index) => DottedBorder(
+                      child: Container(
+                        child: Center(child: _boxContents[index]),
+                        decoration: index <= imageFileList!.length - 1
+                            ? BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: FileImage(
+                                        File(imageFileList![index].path))))
+                            : null,
+                      ),
+                      color: Colors.grey,
+                      dashPattern: [5, 3],
+                      borderType: BorderType.RRect,
+                      radius: Radius.circular(10))).toList(),
             ),
             Container(
               width: 110,
